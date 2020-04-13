2ef2e98a644e3d6cf2a4eb117bbe171559830116
Sergey Tokar <tokarsergey1988@gmail.com>
2020-04-13 23:43:40 +0300

    feat: add basic routing and several pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'shopping-cart',
+    component: ShoppingCartComponent,
+  },
+  {
+    path: 'catalog',
+    component: CatalogComponent,
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+  },
+  {
+    path: '**', 
+    redirectTo: '/home',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    ShoppingCartComponent,
+    CatalogComponent,
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
