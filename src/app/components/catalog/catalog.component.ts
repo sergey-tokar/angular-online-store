babae83ce7f218bf22f3192496c6891a59f6401c
Sergey Tokar <tokarsergey1988@gmail.com>
2020-04-19 15:34:42 +0300

    feat: add html and css catalog component with items

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page-catalog',
+  selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss']
 })
